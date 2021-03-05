1a81e5c7e8e85b20b7972dc94fb0a7e6ee9404ff
nsun9505 <nsun9505@daum.net>
2021-03-05 23:00:35 +0900

    프로젝트 생성

---
+++ src/main/java/com/sarangsi/libseat/reserv/Library/Integrated/Seat/Reservation/Service/LibraryIntegratedSeatReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sarangsi.libseat.reserv.Library.Integrated.Seat.Reservation.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryIntegratedSeatReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryIntegratedSeatReservationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarangsi/libseat/reserv/Library/Integrated/Seat/Reservation/Service/LibraryIntegratedSeatReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarangsi.libseat.reserv.Library.Integrated.Seat.Reservation.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryIntegratedSeatReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
